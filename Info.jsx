9a82d7554c38e5cf77b0b3ef82c151714ba1cf04
MufeedBashir <84316713+MufeedBashir@users.noreply.github.com>
2021-06-27 01:31:31 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="info">
+      <h3>JavaScript & ReactJS</h3>
+      <p>'This was an amazing Bootcamp taken up by Shaurya Sir.'</p>
+      <p>"I would Love to learn more from him ahead."</p>
+      <p>'Thank You! Shaurya Sir and ShapeAI Team as well.'</p>
+    </div>
+  );
+}
+export default Info;
